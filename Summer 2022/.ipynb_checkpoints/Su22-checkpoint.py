5669a7d769865b75154bdd239e444c03c3df7a98
Fanuel <fmammo1@jh.edu>
2022-06-27 07:55:05 -0400

    tweaks to tomography and change of backend

--- Summer 2022/.ipynb_checkpoints/Su22-checkpoint.py
+++ Summer 2022/.ipynb_checkpoints/Su22-checkpoint.py
@@ -20,14 +20,6 @@ from scipy.optimize import curve_fit
 warnings.filterwarnings('ignore')
 from qiskit.tools.jupyter import *
 
-token = 'b6464d13b284902ed1d1a48d2aed6bd0474c7be45011741b0fb879614419659cb722e74a046af3d5caae0398aec9bdac8843068ecbee91aff466cf3e30f3bef5'
-try:
-    IBMQ.load_account()
-except:
-    qk.IBMQ.save_account(token=token)
-    qk.IBMQ.enable_account(token)
-provider = IBMQ.get_provider(hub="ibm-q", group="open", project="main")
-backend = provider.get_backend("ibmq_armonk")## 
 
 def fit_function(x_values, y_values, function, init_params):
     fitparams, conv = curve_fit(function, x_values, y_values, init_params)
@@ -54,29 +46,29 @@ class Custom_Fgp:
             for j in range(int(self.length)):
                 temp.append(i)
         temp = np.array(temp)
-        with pulse.build(backend=backend, default_alignment='sequential', name='Rabi Experiment') as custom_Pulse:
+        with pulse.build(backend=self.backend, default_alignment='sequential', name='Rabi Experiment') as custom_Pulse:
             [pulse.play(temp*self.pi_p, pulse.drive_channel(0))]
         return custom_Pulse
     
     def Customize_pulse(self,x):
-        with pulse.build(backend=backend, default_alignment='sequential', name='Rabi Experiment') as custom_Pulse:
+        with pulse.build(backend=self.backend, default_alignment='sequential', name='Rabi Experiment') as custom_Pulse:
             [pulse.play(self.norm*x, pulse.drive_channel(0))]
             pulse.measure(qubits=[0], registers=[pulse.MemorySlot(0)])
         return custom_Pulse
     
-    def Customize_pulse_2(self, x, length ):
+    def Customize_pulse_2(self, x, length):
         temp = []
         for i in self.norm:
             for j in range(int(length)):
                 temp.append(i)
         temp = np.array(temp)
-        with pulse.build(backend=backend, default_alignment='sequential', name='Rabi Experiment') as custom_Pulse:
+        with pulse.build(backend=self.backend, default_alignment='sequential', name='Rabi Experiment') as custom_Pulse:
             [pulse.play(temp*x, pulse.drive_channel(0))]
             pulse.measure(qubits=[0], registers=[pulse.MemorySlot(0)])
         return custom_Pulse
     
     def draw(self):
-        return self.Create_Pulse_2(1).draw(backend=self.backend)
+        return self.Customize_pulse_2(1,self.length).draw(backend=self.backend)
     
     def baseline_remove(self, values):
         return np.array(values) - np.mean(values)
@@ -105,16 +97,16 @@ class Custom_Fgp:
 
         return drive_amps,rabi_values'''
     
-    def Cali(self,num_rabi_points):
+    def Cali(self,num_rabi_points,length):
         scale_factor = 1e-15
         drive_amp_min = -1
         drive_amp_max = 1
         drive_amps = np.linspace(drive_amp_min, drive_amp_max, num_rabi_points)
 
-        rabi_schedules = [self.Customize_pulse_2(a,self.length) for a in drive_amps]
+        rabi_schedules = [self.Customize_pulse_2(a,length) for a in drive_amps]
         #return rabi_schedules
         num_shots_per_point = 1024
-        job = backend.run(rabi_schedules, 
+        job = self.backend.run(rabi_schedules, 
                   meas_level=1, 
                   meas_return='avg', 
                   shots=num_shots_per_point)
@@ -142,20 +134,20 @@ class Custom_Fgp:
             counter+=1
             if counter==100:
                 break
-            drive_amps,rabi_values = self.rabi_test_Sim(num_rabi_points,counter)
+            drive_amps,rabi_values = self.Cali(num_rabi_points,counter)
             fit_params, y_fit = fit_function(drive_amps,
                                  rabi_values, 
                                  lambda x, A, B, drive_period, phi:(A*np.cos(2*np.pi*x/drive_period - phi) + B),
-                                 [-8, 1, 2, 0])
+                                 [-8, 1, 0.6, 0])
             drive_period = fit_params[2]
             pi_amp = abs(drive_period / 2)
             print(counter, ": ", pi_amp)
         self.length = counter
-        drive_amps,rabi_values = self.Cali(50)
+        drive_amps,rabi_values = self.Cali(50,counter)
         fit_params, y_fit = fit_function(drive_amps,
                                  rabi_values, 
                                  lambda x, A, B, drive_period, phi:(A*np.cos(2*np.pi*x/drive_period - phi) + B),
-                                 [-8, 1, 2, 0])
+                                 [-8, 1, 0.6, 0])
         drive_period = fit_params[2]
         pi_amp = abs(drive_period / 2)
         plt.scatter(drive_amps, rabi_values, color='black')
@@ -181,7 +173,7 @@ class Custom_Fgp:
         rabi_schedules = [self.Customize_pulse_2(1,a) for a in drive_len]
         #return rabi_schedules
         num_shots_per_point = 1024
-        job = backend.run(rabi_schedules, 
+        job = self.backend.run(rabi_schedules, 
                   meas_level=1, 
                   meas_return='avg', 
                   shots=num_shots_per_point)
